501c3183728aa8a958a72c0ca0aee9a41eb420e9
Avi Paz <pazavi@h-iMac-sl-Avi.local>
2021-01-14 12:58:16 +0200

    scss1

--- frontend/src/cmps/card/CardPreview.jsx
+++ frontend/src/cmps/card/CardPreview.jsx
@@ -3,7 +3,7 @@ export function CardPreview(props) {
     return (
         <article className="card-preview">
             <p>{card.title}</p>
-            <p>This is group ID {groupId}</p>
+            {/* <p>This is group ID {groupId}</p> */}
             
         </article>
     )
--- frontend/src/cmps/group/GroupPreview.jsx
+++ frontend/src/cmps/group/GroupPreview.jsx
@@ -5,9 +5,11 @@ export function GroupPreview(props) {
     const { group } = props
     return (
         <article className="group-preview">
+            <div className="group-wraper">
             <p>{group.title}</p>
             {group.cards && <CardList groupId={group.id} cards={group.cards} />}
             <p>Add new Card</p>
+            </div>
         </article>
     )
 }
